9cadad18fa2a19dcf1610912e1235496b8cc6738
Kristo90 <96153349+Kristo90@users.noreply.github.com>
2023-10-06 19:28:02 -0300

    poner ejecutable al mian

--- src/vacunar23_grupo79/VacunAR23_grupo79.java
+++ src/vacunar23_grupo79/VacunAR23_grupo79.java
@@ -2,6 +2,7 @@ package vacunar23_grupo79;
 
 import java.sql.Connection;
 import vac.Conexion.MiConexion;
+import vac.Vistas.frmPrincipal;
 
 
 public class VacunAR23_grupo79 {
@@ -11,7 +12,9 @@ public class VacunAR23_grupo79 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Connection con = MiConexion.getConexion();
+         frmPrincipal pri = new frmPrincipal();
+         
+         pri.setVisible(true);
     }
     
 }
